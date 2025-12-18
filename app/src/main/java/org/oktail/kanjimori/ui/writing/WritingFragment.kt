125f35601428d7500dd9be07298d0fc663f01871
LECOQ Vincent <110967411+oktailb@users.noreply.github.com>
2025-12-18 21:51:10 +0900

    remove useless button

--- app/src/main/java/org/oktail/kanjimori/ui/writing/WritingFragment.kt
+++ app/src/main/java/org/oktail/kanjimori/ui/writing/WritingFragment.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import org.oktail.kanjimori.R
 import org.oktail.kanjimori.databinding.FragmentWritingBinding
 
+data class LevelInfo(val button: Button, val xmlName: String, val stringResId: Int)
+
 class WritingFragment : Fragment() {
 
     private var _binding: FragmentWritingBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var levelInfos: List<LevelInfo>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,28 +31,34 @@ class WritingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initializeLevelInfos()
         setupClickListeners()
     }
 
+    private fun initializeLevelInfos() {
+        levelInfos = listOf(
+            LevelInfo(binding.buttonWritingN5, "N5", R.string.level_n5),
+            LevelInfo(binding.buttonWritingN4, "N4", R.string.level_n4),
+            LevelInfo(binding.buttonWritingN3, "N3", R.string.level_n3),
+            LevelInfo(binding.buttonWritingN2, "N2", R.string.level_n2),
+            LevelInfo(binding.buttonWritingN1, "N1", R.string.level_n1),
+            LevelInfo(binding.buttonWritingClass1, "Grade 1", R.string.level_class_1),
+            LevelInfo(binding.buttonWritingClass2, "Grade 2", R.string.level_class_2),
+            LevelInfo(binding.buttonWritingClass3, "Grade 3", R.string.level_class_3),
+            LevelInfo(binding.buttonWritingClass4, "Grade 4", R.string.level_class_4),
+            LevelInfo(binding.buttonWritingClass5, "Grade 5", R.string.level_class_5),
+            LevelInfo(binding.buttonWritingClass6, "Grade 6", R.string.level_class_6),
+            LevelInfo(binding.buttonWritingTest4, "Grade 7", R.string.level_high_school_1),
+            LevelInfo(binding.buttonWritingTest3, "Grade 8", R.string.level_high_school_2),
+            LevelInfo(binding.buttonWritingTestPre2, "Grade 9", R.string.level_high_school_3),
+            LevelInfo(binding.buttonWritingTest2, "Grade 10", R.string.level_high_school_4),
+        )
+    }
+
     private fun setupClickListeners() {
-        binding.buttonWritingN5.setOnClickListener { navigateToRecap("N5") }
-        binding.buttonWritingN4.setOnClickListener { navigateToRecap("N4") }
-        binding.buttonWritingN3.setOnClickListener { navigateToRecap("N3") }
-        binding.buttonWritingN2.setOnClickListener { navigateToRecap("N2") }
-        binding.buttonWritingN1.setOnClickListener { navigateToRecap("N1") }
-
-        binding.buttonWritingClass1.setOnClickListener { navigateToRecap("Grade 1") }
-        binding.buttonWritingClass2.setOnClickListener { navigateToRecap("Grade 2") }
-        binding.buttonWritingClass3.setOnClickListener { navigateToRecap("Grade 3") }
-        binding.buttonWritingClass4.setOnClickListener { navigateToRecap("Grade 4") }
-        binding.buttonWritingClass5.setOnClickListener { navigateToRecap("Grade 5") }
-        binding.buttonWritingClass6.setOnClickListener { navigateToRecap("Grade 6") }
-
-        // Assuming these buttons correspond to Kanji Kentei levels.
-        // Note: "Kanken Pre-2" is not defined in your kanji_levels.xml, so no listener is set for it.
-        binding.buttonWritingTest4.setOnClickListener { navigateToRecap("Level 4") }
-        binding.buttonWritingTest3.setOnClickListener { navigateToRecap("Level 3") }
-        binding.buttonWritingTest2.setOnClickListener { navigateToRecap("Level 2") }
+        for (info in levelInfos) {
+            info.button.setOnClickListener { navigateToRecap(info.xmlName) }
+        }
     }
 
     private fun navigateToRecap(level: String) {
