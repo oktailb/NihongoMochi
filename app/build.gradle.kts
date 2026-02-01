476b98c89dd7ee704f61c91ad515baf4ea54aae6
LECOQ Vincent <110967411+oktailb@users.noreply.github.com>
2026-02-01 10:30:34 +0900

    version 0.9.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "org.nihongo.mochi"
         minSdk = 28
         targetSdk = 36
-        versionCode = 25
-        versionName = "0.9.1"
+        versionCode = 26
+        versionName = "0.9.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
