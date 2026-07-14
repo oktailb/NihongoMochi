604108bab5d8a8d58e82058ac0949298f90126f4
LECOQ Vincent <110967411+oktailb@users.noreply.github.com>
2026-07-14 10:18:46 +0900

    improve grammar tree and lessons display to match original

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1229266543" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4045615197" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
