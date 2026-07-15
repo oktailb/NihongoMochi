7487fae7d9d5793bb853529b6de1dee9ce603693
LECOQ Vincent <110967411+oktailb@users.noreply.github.com>
2026-07-15 09:29:27 +0900

    minor fixes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2980131732" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "895132149" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
